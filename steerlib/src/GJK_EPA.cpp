8f51f7baa7c54719069bd148fa0eb99dc37fef8f
gqxcd <gaoqi2103@gmail.com>
2016-10-08 14:13:42 -0400

    commit for A2

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -1,3 +1,10 @@
+/*!
+*
+* \author VaHiD AzIzI
+*
+*/
+
+
 #include "obstacles/GJK_EPA.h"
 
 
@@ -8,5 +15,5 @@ SteerLib::GJK_EPA::GJK_EPA()
 //Look at the GJK_EPA.h header file for documentation and instructions
 bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
 {
-	return false; // There is no collision
+    return false; // There is no collision
 }
